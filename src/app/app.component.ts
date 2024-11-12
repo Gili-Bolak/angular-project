f73894af7fb593a8fe4fdc87889bdecd1939370e
gili-dev <g0583398@gmail.com>
2024-11-12 20:23:53 +0200

    add task and user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
   users = DUMMY_USERS
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find((user)=> user.id === this.selectedUserId)
+  }
 
   onSelectUser(id: string){
-    console.log("id: " + id);
-    
+    this.selectedUserId = id; 
   }
 }
